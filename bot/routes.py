760538705d9a0e01cbe642056416ded6c5129f2a
Pragun Bajracharya <pragunbaj99@gmail.com>
2021-12-21 19:30:19 +0545

    Added skype bot files

---
+++ bot/routes.py
@@ -0,0 +1,31 @@
+from bot import app
+import os
+import json
+from dotenv import load_dotenv
+from skpy import Skype
+from flask import Flask, request, jsonify
+
+load_dotenv()
+
+
+@app.route("/sendMessage", methods=['GET', 'POST'])
+def sendMessage():
+    if request.method == 'GET':
+        return {'status': 'error', 'message': 'Bad Request'}
+    if request.method == 'POST':
+        content_type = request.headers.get('Content-Type')
+        if content_type != 'application/json':
+            return {'status': 'error', 'message': 'Content-Type not supported!'}
+        data = json.loads(request.data)
+        # if data.has_key('username') and data.has_key('message'):
+        if 'username' in data.keys() and 'message' in data.keys():
+            username = data['username']
+            message = data['message']
+            try:
+                sk = Skype(os.getenv('email'), os.getenv('password'))
+                ch = sk.contacts[username].chat
+                ch.sendMsg(message)
+                return {'status': 'success', 'message': 'Message sent successfully.'}
+            except:
+                return {'status': 'error', 'message': 'Message not sent.'}
+        return {'status': 'error', 'message': 'Missing parameter.'}
